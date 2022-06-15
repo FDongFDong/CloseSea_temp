3ceedd84e735525290d82097b3e27548d99b3d93
FdongFdong <abnormal5626@naver.com>
2022-06-15 10:36:08 +0900

    상준님꺼랑 합침

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import About from './pages/About';
 import Navigation from './components/Navigation';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Main from './pages/Main';
 import Stats from './pages/Stats';
 import Explore from './pages/Explore';
 import Create from './pages/Create';
 import Wallet from './pages/Wallet';
 import Resources from './pages/Resources';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <>
       <Router>
-        <Navigation></Navigation>
+        <Nav></Nav>
         {/* 1. Switch -> Routes */}
 
         <Routes>
           {/* 2. component -> element, element형태로 넘겨야함 <Home /> */}
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Main />} />
 
           <Route path="/create" element={<Create />} />
           <Route path="/explore" element={<Explore />} />
--- src/pages/Home.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const Home = () => {
-  return <div>여긴 홈</div>;
-};
-
-export default Home;
